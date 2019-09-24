0804b39ad68a7e7763d6d4c3ed327e224c117f91
Michael Yue <michaelyue123@gmail.com>
2019-09-24 19:56:15 +1000

    Merge branch 'master' of https://github.com/duanxinhuan/S-E-RealEstate
     Conflicts:
    	out/production/Realestate/application/LinkDatabase.class
    	out/production/Realestate/application/RealEmployee.class
    	out/production/Realestate/application/RealEstate.class
    	out/production/Realestate/tests/BuyerTest.class
    	src/application/RealEstate.java

---
+++ src/property/Application.java
@@ -0,0 +1,24 @@
+package property;
+
+public class Application implements Applicant {
+
+    private String name;
+    private String occupation;
+    private double income;
+    private double contractLength;
+    private double contractPrice;
+    private String status;
+
+    public Application() {
+    }
+
+    @Override
+    public void accept() {
+
+    }
+
+    @Override
+    public void reject() {
+
+    }
+}
