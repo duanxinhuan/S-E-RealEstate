580d66f463d11316c80cddcfe8774113900cf2f1
FROZENPRAWN <53968459+FROZENPRAWN@users.noreply.github.com>
2019-10-16 00:38:53 +1100

    Update Vendor.java

--- src/customer/Vendor.java
+++ src/customer/Vendor.java
@@ -2,22 +2,46 @@ package customer;
 
 import property.Property;
 
+
 public class Vendor extends VLCustomers {
+    private String statusOfOffer[];
+    private long time;
+    private long advance;
+    private int ConsiderationTime;
+    private double minPrice;
 
 
-    int numOfSell = 0;
 
-    public Vendor(String customerId, String passWord, String name, String emailAddress) {
-        super(customerId, passWord, name, emailAddress);
+    public Vendor(String customerId, String passWord, String name, String emailAddress, String[] statusOfOffer) {
+        super ( customerId, passWord, name, emailAddress );
+        this.statusOfOffer = new String[]{"accepted","rejected"};
     }
 
-    public void addProperty(Property p) {
-        numOfSell+=1;
+
+    public void setConsiderationTime(int considerationTime) {
+        ConsiderationTime = considerationTime;
     }
 
+    public void setStatusOfOffer(String[] statusOfOffer) {
+        this.statusOfOffer = statusOfOffer;
+    }
+
+    public void DateTime(int considerationTime ) {
+        advance = ((considerationTime * 24L)*60L)*60L*1000L;
+        time = System.currentTimeMillis()+advance;
+    }
+
+    public void setMinPrice(double minPrice) {
+        this.minPrice = minPrice;
+    }
+
+
+//    int numOfSell = 0;
+//    public void addProperty(Property p) {
+//        numOfSell+=1;
+//    }
 
 }
-
 
 
 
